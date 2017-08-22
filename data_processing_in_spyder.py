bade25af9b6fbf665274973c5f48c8442f662aed
iterater <sergey.v.kovalchuk@gmail.com>
2017-08-23 01:32:33 +0300

    Testing HMM basics

--- data_processing_in_spyder.py
+++ data_processing_in_spyder.py
@@ -1,13 +1,35 @@
 import os
 import codecs
 import re
+import seaborn as sns
+import numpy as np
+import hmmlearn.hmm as hmm
+from sklearn.externals import joblib
 
-data_dir = 'D:\Src\health-hmm\data'
+data_dir = 'data'
+lengthes = []
+all_state = []
+all_los = []
 with codecs.open(os.path.join(data_dir, 'Clusters_with_durations.txt'), encoding='utf-8') as f:
     for line in f:
         if re.match(r'\[(.*)\]', line):
             states = [m.group(1) for m in re.finditer(r'\'([A-Z]*)\'', line)]
             duration = [int(m.group(1)) for m in re.finditer(r'[\s,]+([0-9]+)[,\]]+', line)]
-            print(states, duration)
-            # s = re.search(r'\[(.*)\]', line).group(1)
-            # observations = np.array(list(map(float, re.split(r'\s*,\s*', s))), dtype=float)
+            all_state.extend(states)
+            all_los.extend(duration)
+            lengthes.append(len(states))
+            print(list(zip(states, duration)))
+lengthes = np.array(lengthes, dtype=int)
+all_los = np.array(all_los, dtype=int) // 15 # 15-min intervals
+all_state = np.array(all_state, dtype=str)
+all_state_index = np.unique(all_state)
+all_state = np.array(list(map(lambda a: np.where(all_state_index == a)[0][0], 
+                              all_state)), dtype=int)
+
+x = np.transpose([all_state, all_los])
+
+m = hmm.GaussianHMM(n_components=3)
+m.fit(x, lengths=lengthes)
+
+joblib.dump(m, 'test_model.pkl')
+# TODO: discrete-events -> discrete-time
