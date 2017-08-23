60a294946d02d5f9378f5ebc81a7254ce009dfa0
Sergey V. Kovalchuk <sergey.v.kovalchuk@gmail.com>
2017-08-23 20:34:33 +0300

    Testing HMM

--- data_processing_in_spyder.py
+++ data_processing_in_spyder.py
@@ -26,7 +26,7 @@ all_state = np.array(all_state, dtype=str)
 all_state_index = np.unique(all_state)
 all_state_id = np.array(list(map(lambda a: np.where(all_state_index == a)[0][0], all_state)), dtype=int)
 
-# building sequence with q
+# building sequence with quantiles
 los_quartiles = dict((aidx, np.percentile(all_los[all_state == aidx], [0, 25, 50, 75])) for aidx in all_state_index)
 all_q_state = []
 all_q_length = []
@@ -49,6 +49,8 @@ for i in range(len(all_state)):
         t_los_case_idx += 1        
 all_q_state = np.array(all_q_state)
 all_q_state_index = np.unique(all_q_state)
+
+# testing simple Markov process
 transition_count = np.zeros((len(all_q_state_index), len(all_q_state_index)))
 for i in range(1, len(all_q_state)):
     if not all_q_case_start_mask[i]:
@@ -56,7 +58,7 @@ for i in range(1, len(all_q_state)):
         i2 = np.where(all_q_state_index == all_q_state[i])
         transition_count[i1, i2] +=1
 
-# saving normed edges
+# saving and plotting normed edges
 tcn = transition_count / np.sum(transition_count, axis=1)[:, None]
 with open('edges.csv', 'w') as f:
     f.write('Source;Target;Weight\n')
@@ -65,15 +67,30 @@ with open('edges.csv', 'w') as f:
             if tcn[i,j] > 0:
                 f.write('{0};{1};{2}\n'.format(all_q_state_index[i], all_q_state_index[j], tcn[i, j]))
     f.close()
+plt.figure(figsize=(5,5))
+plt.xticks(np.arange(len(all_q_state_index)), all_q_state_index, rotation='vertical')
+plt.yticks(np.arange(len(all_q_state_index)), all_q_state_index)
+plt.imshow(tcn, cmap=plt.cm.Greys)
 
 # training HMM
-N = 5
 all_q_state_id = np.array(list(map(lambda a: np.where(all_q_state_index == a)[0][0], all_q_state)), dtype=int)
+N = 24
 m = hmm.MultinomialHMM(n_components=N)
 m.fit(all_q_state_id.reshape(-1, 1), lengths=all_q_length)
+print(N, m.score(all_q_state_id.reshape(-1, 1), lengths=all_q_length),
+      m.monitor_.converged)
 
 # plotting results
+plt.figure(figsize=(5,5))
 plt.xticks(np.arange(len(all_q_state_index)), all_q_state_index, rotation='vertical')
 plt.yticks(np.arange(N))
-plt.imshow(m.emissionprob_, cmap=plt.cm.hot)
+plt.imshow(m.emissionprob_, cmap=plt.cm.Greys)
 
+plt.figure(figsize=(5,5))
+plt.xticks(np.arange(N))
+plt.yticks(np.arange(N))
+plt.imshow(m.transmat_, cmap=plt.cm.Greys)
+
+# TODO: Deordertransition matrix
+# TODO: Compare next step prediction with simple MM and HMM
+# TODO: Ensemble management and HMM
